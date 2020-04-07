de9bcf87382746506788c887a51c3538d6c7b7b0
Jsnakamura <juliano.s.nakamura@hotmail.com>
2020-04-06 22:46:10 -0300

    Init Project

---
+++ winesale/src/main/java/com/ubots/winesale/WineSaleApplication.java
@@ -0,0 +1,13 @@
+package com.ubots.winesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineSaleApplication.class, args);
+	}
+
+}
---
+++ winesale/src/test/java/com/ubots/winesale/WineSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubots.winesale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
